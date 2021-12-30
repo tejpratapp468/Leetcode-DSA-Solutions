15a79ef5e0d29718db4760926f91f4725a752c12
tejpratapp468 <58838640+tejpratapp468@users.noreply.github.com>
2021-12-30 19:01:21 +0530

    Update 1015. Smallest Integer Divisible by K.cpp

--- 1015. Smallest Integer Divisible by K.cpp
+++ 1015. Smallest Integer Divisible by K.cpp
@@ -4,7 +4,7 @@ public:
     //Space Complexity O(1)
     int smallestRepunitDivByK(int k) {
         int unitD=k%10;
-        if(unitD==0 || unitD==2 || unitD==4 || unitD==6 || unitD==8) return -1;
+        if(unitD==0 || unitD==2 || unitD==4 || unitD==6 || unitD==8) return -1;  //we can write line 7 of our code as: if(k%2==0) return -1;
         if(k==1) return 1;
         long rem=0;
         for(int i=1;i<=k;i++)
